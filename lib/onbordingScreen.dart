ef50aeaa22a124bccefe112dbe68845bedecfec8
Jerry <jerry@codberg.com>
2024-07-30 10:12:24 +0900

    onbordingScreen

---
+++ lib/onbordingScreen.dart
@@ -0,0 +1,13 @@
+class onbordingScreen extends StatefulWidget {
+  const onbordingScreen({super.key});
+
+  @override
+  State<onbordingScreen> createState() => _onbordingScreenState();
+}
+
+class _onbordingScreenState extends State<onbordingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
